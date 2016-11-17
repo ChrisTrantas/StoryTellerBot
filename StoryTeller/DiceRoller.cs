1fc3c3b027ce619e72ef7dff67f3f4bd9eb072cb
Chris Trantas <cmt4536@rit.edu>
2016-11-17 00:06:52 -0500

    Initial Commit. Has Discord Client and Commands. Has a simple dice roller, a forage command which will most likely be changed, and a some time conversions from EST to other US time zone commands.

---
+++ StoryTeller/DiceRoller.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTeller
+{
+    partial class MyBot
+    {
+        // 1. User types what roll to use ex !roll 6
+        // 2. User types how many dice to use ex !roll 5 6
+        // 3. Advantage ex !roll adv/dis
+        // 4. Advantage with mod. !roll adv/dis 5
+        // 5. Stats
+        partial void RegisterRollDiceCommand()
+        {
+
+            int roll, dieSize, mod, advRoll;
+            string die, modifier;
+
+            commands.CreateCommand("roll")
+                .Parameter("roll", Discord.Commands.ParameterType.Required)
+                .Do(async (e) =>
+                {
+                    die = e.GetArg("roll");
+                    int.TryParse(die, out dieSize);
+                    roll = rand.Next(1, dieSize + 1);
+                    await e.Channel.SendMessage("You rolled a **" + roll.ToString() + "**");
+                });   
+
+            commands.CreateCommand("adv")
+                .Parameter("modifier", Discord.Commands.ParameterType.Required)
+                .Do(async (e) =>
+                {
+                    modifier = e.GetArg("modifier");
+                    int.TryParse(modifier, out mod);            
+                    roll = rand.Next(1, 21);
+                    advRoll = rand.Next(1, 21);
+                    if (roll >= advRoll)                    
+                        roll = roll + mod;                    
+                    else                    
+                        roll = advRoll + mod;
+                    
+                    await e.Channel.SendMessage("You rolled a **" + roll.ToString() + "** with your advantage");
+                    await e.Channel.SendMessage(" ADV Roll: " + advRoll.ToString() + " Mod: " + mod.ToString());
+                });
+
+            commands.CreateCommand("dis")
+                .Parameter("modifier")
+                .Do(async (e) =>
+                {
+                    modifier = e.GetArg("Modifier");
+                    int.TryParse(modifier, out mod);
+                    roll = rand.Next(1, 21);
+                    advRoll = rand.Next(1, 21);
+                    if (roll <= advRoll)
+                        roll = roll + mod;
+                    else
+                        roll = advRoll + mod;
+                    await e.Channel.SendMessage("You rolled a **" + roll.ToString() + "** with your disadvantage");
+                    await e.Channel.SendMessage("Roll: " + roll.ToString() + " ADV Roll: " + advRoll.ToString() + " Mod: " + mod.ToString());
+                });
+        }
+    }
+}
---
+++ StoryTeller/MyBot.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTeller
+{
+    public partial class MyBot
+    {
+        // Discord
+        DiscordClient discord;
+        CommandService commands;
+        Random rand;
+
+        public MyBot()
+        {
+            rand = new Random();
+            discord = new DiscordClient(x =>
+           {
+               x.LogLevel = LogSeverity.Info;
+               x.LogHandler = Log;
+           });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;               
+            });
+
+            commands = discord.GetService<CommandService>();
+
+            #region RegisterCommandList
+            RegisterRollDiceCommand();
+            test();
+            #endregion
+
+            discord.ExecuteAndWait(async () => 
+            {
+                await discord.Connect("MjQ3NzE4NTcwNjUyMzM2MTM4.CwtTnw.cieBhKa3M5INA9JpYt3rh_yexcs"
+, TokenType.Bot);
+            });
+
+           
+        }
+
+        partial void RegisterRollDiceCommand();
+        private void test()
+        {
+            commands.CreateCommand("Test")
+                .Do(async (e) =>
+            {
+                await e.Channel.SendMessage("It works. Your other code is fucked.");
+            });
+        }
+        
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
